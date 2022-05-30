862fc232865377f3a06c62e47bbeb6d9c9803800
obfuscatedgenerated <75035364+obfuscatedgenerated@users.noreply.github.com>
2022-05-30 01:08:51 +0100

    color changing

--- challenge_1/butterfly.py
+++ challenge_1/butterfly.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.collections import LineCollection
 import numpy as np
 
 domain = np.arange(0, 12 * np.pi, 0.01)
@@ -9,6 +11,18 @@ y = np.cos(domain) * ((np.e ** np.cos(domain)) - 2 * np.cos(4 * domain) - (np.si
 fig, ax = plt.subplots()
 
 ax.plot(x,y)
+
+line_segs = []
+r = np.sqrt(x**2 + y**2)
+
+
+for i in range(len(x) - 1):
+    line_segs.append([(x[i], y[i]), (x[i+1], y[i+1])])
+
+color = LineCollection(line_segs, cmap=cm.rainbow)
+color.set_array(r)
+ax.add_collection(color)
+
 
 ax.set(title="Butterfly", xlabel="x", ylabel="y")
 ax.set_aspect("equal", adjustable="box")
--- challenge_1/fermat_spiral.py
+++ challenge_1/fermat_spiral.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.collections import LineCollection
 import numpy as np
 
 domain = np.arange(0, 16 * np.pi, 0.01)
@@ -9,6 +11,17 @@ y = np.sqrt(domain) * np.sin(domain)
 fig, ax = plt.subplots()
 
 ax.plot(x,y)
+
+line_segs = []
+r = np.sqrt(x**2 + y**2)
+
+
+for i in range(len(x) - 1):
+    line_segs.append([(x[i], y[i]), (x[i+1], y[i+1])])
+
+color = LineCollection(line_segs, cmap=cm.Blues_r)
+color.set_array(r)
+ax.add_collection(color)
 
 ax.set(title="Fermat's Spiral", xlabel="x", ylabel="y")
 ax.set_aspect("equal", adjustable="box")
