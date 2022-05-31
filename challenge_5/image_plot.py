03089c074e8c9bfec1c699e810fad7d108d78212
obfuscatedgenerated <75035364+obfuscatedgenerated@users.noreply.github.com>
2022-05-31 22:08:07 +0100

    subtle formatting

--- challenge_5/image_plot.py
+++ challenge_5/image_plot.py
@@ -12,9 +12,9 @@ image_negative = 255 - image # invert the image
 #fig = plt.figure() # create figure
 
 fig, axes = plt.subplots(2, 2) # create figure
-axes[0,0] = plt.subplot(2,2,1) # override subplot
-axes[0,1] = plt.subplot(2,2,2) # override subplot
-axes[1,0] = plt.subplot(2,1,2) # override subplot with double width
+axes[0,0] = plt.subplot(2, 2, 1) # override subplot
+axes[0,1] = plt.subplot(2, 2, 2) # override subplot
+axes[1,0] = plt.subplot(2, 1, 2) # override subplot with double width
 axes[1,1] = None # override subplot with None
 
 axes[0,0].imshow(image, interpolation="bilinear") # create image in matplotlib
@@ -27,7 +27,7 @@ for channel, color in enumerate(["r", "g", "b"]): # iterate over channels
     histr = cv2.calcHist([image], [channel], None, [256], [0, 256]) # calculate histogram
     axes[1,0].plot(histr, color=color) # plot histogram
 
-axes[1,0].set_xlim([0,256])
+axes[1,0].set_xlim([0, 256])
 
 fig.canvas.manager.set_window_title("Challenge 5.1: Image Plotting") # set the window title
 
