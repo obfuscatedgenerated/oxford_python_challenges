8c3afe25ae08993563531a1ea13bfef761288160
obfuscatedgenerated <75035364+obfuscatedgenerated@users.noreply.github.com>
2022-05-31 22:05:26 +0100

    fix cv2 conversion

--- challenge_5/image_plot.py
+++ challenge_5/image_plot.py
@@ -6,6 +6,7 @@ from os import path
 IMAGE_PATH = path.join(path.dirname(__file__), "sea.jpg")
 
 image = cv2.imread(IMAGE_PATH) # read image
+image = image[:,:,::-1] # convert to RGB, cv2 reads in BGR
 image_negative = 255 - image # invert the image
 
 #fig = plt.figure() # create figure
@@ -22,7 +23,7 @@ axes[0,0].axis("off")
 axes[0,1].imshow(image_negative, interpolation="bilinear") # create image in matplotlib
 axes[0,1].axis("off")
 
-for channel, color in enumerate(["b", "g", "r"]): # iterate over channels
+for channel, color in enumerate(["r", "g", "b"]): # iterate over channels
     histr = cv2.calcHist([image], [channel], None, [256], [0, 256]) # calculate histogram
     axes[1,0].plot(histr, color=color) # plot histogram
 
